5689e3096ad6ac8675bcb56d252e6c5af53ab40c
David <david.cabello.info@gmail.com>
2021-11-22 13:24:46 +0100

    first commit

---
+++ src/main/java/com/everis/everisdesafiogit_uno/App.java
@@ -0,0 +1,13 @@
+package com.everis.everisdesafiogit_uno;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hola Mundo" );
+    }
+}
